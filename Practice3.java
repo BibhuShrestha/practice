eadf31b7962b640e322d624747420be1781b1cd5
Bibhu Shrestha <BibhuShrestha@users.noreply.github.com>
2025-10-07 10:01:14 +0545

    Initial commit

--- Practice3.java
+++ Practice3.java
@@ -1,44 +1,29 @@
+//write a java program to reverse an array.
+
 import java.util.Scanner;
 
 public class Practice3{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter rows: ");
-        int m = sc.nextInt();
-       
-        System.out.print("Enter columns: ");
-        int n = sc.nextInt();
-        int[][] arr1 = new int[m][n];
-        int[][] arr2 = new int[m][n];
-        int[][] result = new int[m][n];
+        System.out.print("Enter the array");
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        
+        
+        System.out.println("Enter" + num + "elements:");
+        for(int i = 0; i< num; i++){
+            arr[i]=sc.nextInt();
+        }
 
-
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                System.out.print("Enter the " + i + "*" + j + " array:");
-                arr1[i][j]=sc.nextInt();
-               
-            }
+        for(int i=0; i<num/2; i++){
+            int temp = arr[i];
+            arr[i] = arr[num-i-1];
+            arr[num-i-1]=temp;
 
         }
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                System.out.print("Enter the " + i + "*" + j + " array:");
-                arr2[i][j]=sc.nextInt();
-            }   
-        }
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                System.out.print("Enter the " + i + "*" + j + " array:");
-                result[i][j] = arr1[i][j] + arr2[i][j];
-            }
-        }
-        System.out.println("Resultant Matrix after Addition:");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(result[i][j] + " ");
-            }
-            System.out.println();
+        System.out.println("Reverse array");
+        for(int i = 0; i<num; i++){
+            System.out.print(arr[i] + " ");
         }
         sc.close();
     }
